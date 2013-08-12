d62ca4e438bfacaf829624f60a634dd0a2b35b9c
Mentessi <mentessi@gmail.com>
2013-08-12 23:10:48 +0100

    user controller specs and view specs created

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe "users/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it "should show all the users details" do 
+  	assign(:users, 
+  		[stub_model(User, :first_name => "michael", 
+  											:email => "michael@example.com",
+  											:created_at => DateTime.new(2001,2,3)),
+  		stub_model(User, 	:first_name => "dave", 
+  											:email => "dave@example.com",
+  											:created_at => DateTime.new(2010,2,3)),
+  		stub_model(User, 	:first_name => "neil", 
+  											:email => "neil@example.com",
+  											:created_at => DateTime.new(2020,2,3))
+  		])
+  	render
+  	rendered.should include("michael")
+  	rendered.should include("signed up 2001-02-03")
+  	rendered.should include("dave")
+  	rendered.should include("signed up 2010-02-03")
+  	rendered.should include("neil")
+  	rendered.should include("signed up 2020-02-03")
+  	
+  end
+
 end
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe "users/show.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should show a users details" do 
+  	assign(:user, stub_model(User, :first_name => "michael", 
+  																	:email => "michael@example.com"))
+  	render
+  	rendered.should include("User: michael")
+  	rendered.should include("Email: michael@example.com")
+  end
 end
