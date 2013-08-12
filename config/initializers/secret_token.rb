69a5dffd0b98b50607c7a9b36e52932b2d487799
Mentessi <mentessi@gmail.com>
2013-08-12 12:21:18 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TakeoutTerror::Application.config.secret_key_base = 'a2158db2595087d75e7acd6a7e493e2135367c58098fabbc85f9251f87c308429640c41a05273cd19843d377499edfb95e6bdbc5b9fcf1bd99b09323fa5a56f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TakeoutTerror::Application.config.session_store :cookie_store, key: '_takeout_terror_session'
