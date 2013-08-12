8001a9c941152d0b89cea4a618821ba0941cad90
Mentessi <mentessi@gmail.com>
2013-08-12 18:24:11 +0100

    added seed file with user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.find_or_create_by_email :first_name => 'first', :last_name => 'user', :email => 'user1@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'user: ' << user1.first_name
+
+user2 = User.find_or_create_by_email :first_name => 'second', :last_name => 'user', :email => 'user2@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'user: ' << user2.first_name
+
+user3 = User.find_or_create_by_email :first_name => 'third', :last_name => 'user',:email => 'user3@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'user: ' << user3.first_name
