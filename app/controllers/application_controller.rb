099c81310dbd4c7c402435760c825911d822c554
Mentessi <mentessi@gmail.com>
2013-08-12 21:59:24 +0100

    fixed strong parameters bug using configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
+	before_filter :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
 
   def after_sign_up_path_for(resource)
   	if resource.is_a?(User)
@@ -14,5 +16,14 @@ class ApplicationController < ActionController::Base
   		user_path(current_user)
   	end
   end
+
+  protected 
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)}
+  end	
+
+
 
 end
