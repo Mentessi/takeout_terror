89c6629abaaf58adfb8eb7abb62275a7b9e8c8ad
Mentessi <mentessi@gmail.com>
2013-09-04 19:15:58 +0100

    user can log in with google

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,8 +14,12 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+
   require "omniauth-facebook"
   config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+
+  require "omniauth-google-oauth2"
+  config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], { access_type: "offline", approval_prompt: "" }
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
---
+++ spec/features/editing_account_feature_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+feature "Editing account" do
+  before(:each) do
+    User.create!( :name => 'Mike Mike', 
+                  :email => 'user34@example.com', 
+                  :password => 'password',
+                  :password_confirmation => 'password')
+    visit '/'
+    click_link 'Login'
+    fill_in 'Email', :with => 'user34@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully'
+  end
+
+  context "with current password provided" do
+    scenario "when changing the email address" do
+      click_link 'Edit account'
+      fill_in 'Email', :with => 'user34@example.com'
+      fill_in 'Current password', :with => 'password'
+      click_button 'Update'
+      expect(page).to have_content 'You updated your account successfully'
+    end
+  end
+
+  context "no password is entered" do
+    scenario "when changing the email address" do
+      click_link 'Edit account'
+      fill_in 'Email', :with => 'user36@example.com'
+      click_button 'Update'
+      expect(page).to have_content '1 error prohibited this user from being saved:'
+      expect(page).to have_content "Current password can't be blank"
+    end
+
+    scenario "when changing password" do
+      click_link 'Edit account'
+      fill_in 'Password', :with => 'newpassword'
+      fill_in 'Password confirmation', :with => 'newpassword'
+      click_button 'Update'
+      expect(page).to have_content 'You updated your account successfully.'
+    end
+
+    scenario "when changing account fields other than password and email" do
+      click_link 'Edit account'
+      fill_in 'Name', :with => 'New Name'
+      click_button 'Update'
+      expect(page).to have_content 'You updated your account successfully.'
+    end
+  end
+end
+
